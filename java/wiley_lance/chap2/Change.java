c65eb487a685e30882139d5e02c05996d2389e37
LanceEddleman <ozzland_dracor@hotmail.com>
2015-10-06 20:14:11 -0600

    update to chapter 2 exercises

--- java/wiley_lance/chap2/Change.java
+++ java/wiley_lance/chap2/Change.java
@@ -18,7 +18,19 @@ public class Change
     // instance variables - replace the example below with your own
     private int x;
     private int userSelection;
+    private String userSelected;
+    private int userQuanity;
+    private int quanityAvailable;
     private int errorCount;
+    private double eggs = 3.49; // = 349 pennies
+    private double bread = 1.99; // = 199 pennies
+    private double cheese = 7.37; // = 737 pennies
+    private int userCoins;
+    private final int dollar = 100;
+    private final int quarter = 25;
+    private final int dimes = 10;
+    private final int nickle = 5;
+    private int pennies;
 
     /**
      * Constructor for objects of class Change
@@ -28,7 +40,12 @@ public class Change
         // initialise instance variables
         x = 0;
         userSelection = 0;
+        userSelected = "";
+        userQuanity = 0;
+        quanityAvailable = 100;
         errorCount = 0;
+        userCoins = 0;
+        pennies = 0;
     }
 
     public static void main(String[] args)
@@ -49,82 +66,52 @@ public class Change
         return x + y;
     }
 
-        public void selection()
+    public void selection()
     {
         items();
+        System.out.printf("Enter the ID of the item you want to purchase.\n");
         selectItem();
-        testSelection();
-    }
-        public void errorSelection()
-    {
-        items();
-        errorSelectItem();
-        testSelection();
     }
 
     public void selectItem()
     {
-        System.out.printf("Enter the number of the item you want to purchase\n");
         Scanner userInput = new Scanner(System.in); //construct scanner
         while(!userInput.hasNextInt()){
-            if (errorCount >= 2)
-            {
-                errorCount = 0;
-                sysClear();
-                selection();
-
-            }
-            errorCount++;
-            System.out.println("Input was not a number");
-            System.out.println("Please make another selection");
+            sysClear();
+            items();
+            notNumber();
             userInput.next(); // input invalid, try again
         }
         int userSelection = userInput.nextInt();
         userInput.close(); //close scanner
         this.userSelection = userSelection;
-        // return userSelection;
-    }
-
-        public void errorSelectItem()
-    {
-        System.out.printf("Enter the number of the item you want to purchase\n");
-        Scanner userInput = new Scanner(System.in); //construct scanner
-        while(!userInput.hasNextInt()){
-            if (errorCount >= 2)
-            {
-                errorCount = 0;
-                sysClear();
-                System.out.println("Input was not a valid item");
-                System.out.println("Please make another selection");                
-                errorSelection();
-
-            }
-            errorCount++;
-            System.out.println("Input was not a number");
-            System.out.println("Please make another selection");
-            userInput.next(); // input invalid, try again
-        }
-        int userSelection = userInput.nextInt();
-        userInput.close(); //close scanner
-        this.userSelection = userSelection;
+        
+        testSelection();
         // return userSelection;
     }
 
     public void testSelection()
     {
-        // selectItem();
-        // userSelection = 4;
         if (userSelection == 1 || userSelection == 2 || userSelection == 3)
         {
             switch (userSelection) {
               case 1:
+                    sysClear();
                     System.out.println("User Selected: Eggs");
+                    this.userSelected = "Eggs";
+                    quanity();
                     break;
               case 2: 
+                    sysClear();
                     System.out.println("User Selected: Bread");
+                    this.userSelected = "Bread";
+                    quanity();
                     break;
               case 3:
+                    sysClear();
                     System.out.println("User Selected: Cheese");
+                    this.userSelected = "Cheese";
+                    quanity();
                     break;
               default:
                     break;
@@ -133,12 +120,128 @@ public class Change
         else
         {
             sysClear();
-            errorSelectItem();
-            selection();
+            items();
+            notValidNumber();
+            selectItem();
         }
         // next step for number of items to purchase
     }
     
+    public void quanity()
+    {
+        items();
+        // userChoice();
+
+        System.out.printf("Enter the quanity of \"%s\" you want to purchase.\n", this.userSelected);
+        selectQuanity();
+    }
+
+    public void selectQuanity()
+    {
+        Scanner userInput = new Scanner(System.in); //construct scanner
+        while(!userInput.hasNextInt()){
+            sysClear();
+            items();
+            notNumber();
+            userInput.next(); // input invalid, try again
+        }
+        int userQuanity = userInput.nextInt();
+        userInput.close(); //close scanner
+        this.userQuanity = userQuanity;
+        
+        testQuanity();
+        // return userSelection;
+    }
+
+    public void testQuanity()
+    {
+        if (userQuanity > 100)
+        {
+            System.out.println("We do not have that much stock on hand");
+            System.out.printf("Please select less then %d.0\n",quanityAvailable);
+            quanity();
+        }
+        else
+        {
+            sysClear();
+            items();
+            System.out.println();
+            
+            selectedItem();
+            initialPrice();
+        }
+        // next step for number of items to purchase
+    }
+
+    public void userChoice()
+    {
+        System.out.printf("Purchse Item: %s\n", userSelection);
+    }
+
+    public void notNumberError()
+    {
+        System.out.println("Input was not a number");
+        System.out.println("Please make another selection");
+    }
+    public void notValidNumberError()
+    {
+        System.out.println("Input was not a valid option");
+        System.out.println("Please make another selection");
+    }
+
+    public void notNumber()
+    {
+        notNumberError();
+    }
+        public void notValidNumber()
+    {
+        notValidNumberError();
+    }
+
+    public void selectedItem()
+    {
+        System.out.printf("You are purchasing: %d, %s\n\n",this.userQuanity, this.userSelected);
+    }
+    public void initialPrice()
+    {
+        double cost = 0;
+        int pennies = 0;
+        double pbase = 0;
+        double premainder = 0;
+        if (userSelected == "Eggs" || userSelected == "Bread" || userSelected == "Cheese")
+            {
+                switch (userSelected) {
+                  case "Eggs":
+                        cost = this.userQuanity * eggs;
+                        break;
+                  case "Bread": 
+                        cost = this.userQuanity * bread;
+                        break;
+                  case "Cheese":
+                        cost = this.userQuanity * cheese;
+                        break;
+                  default:
+                        break;
+                }
+            }
+        // System.out.println("The price will be: " + (double) this.userSelected * this.userQuanity);
+        System.out.println("Total cost is: " + cost);
+
+        System.out.println("We only accept pennies");
+        System.out.println();
+        
+        premainder = (cost*100) % 100;
+        System.out.println("base pennies: " + premainder);
+        pbase = (int)cost*100;
+        System.out.println("base: " + pbase);
+        
+        pennies = (int)pbase+(int)premainder;
+        this.pennies = pennies;
+        
+        System.out.printf("Total cost is: %d pennies\n\n", pennies);
+        customerPrompt();
+    }
+
     public void items()
     {
         String name = "Name";
@@ -151,10 +254,45 @@ public class Change
         System.out.println();
     }
 
+    public void customerPrompt()
+    {
+        System.out.println("Please input your nuber of pennies");
+        customerCoins();
+    }
     
+    public void customerCoins()
+    {
+        Scanner userInput = new Scanner(System.in); //construct scanner
+        while(!userInput.hasNextInt()){
+            sysClear();
+            customerPrompt();
+            notNumber();
+            userInput.next(); // input invalid, try again
+        }
+        int userCoins = userInput.nextInt();
+        userInput.close(); //close scanner
+        this.userCoins = userCoins;
+        
+        displayCustomerCoins();
+        // return userSelection;
+    }
+
+    public void displayCustomerCoins()
+    {
+        int highLow = userCoins - pennies;
+        if (userCoins > pennies)
+        {
+            System.out.printf("You gave me: %d\n", userCoins);
+        }
+        else
+        {
+            System.out.println("Please input a higher amount of pennies");
+            customerCoins();
+        }
+    }
+ 
     public void sysClear()
     {
         System.out.print('\u000C');
     }
 }
-
