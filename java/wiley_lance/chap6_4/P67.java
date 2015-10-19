36053137a7a250a00d2eef919b15178d62dd6fd1
LanceEddleman <ozzland_dracor@hotmail.com>
2015-10-19 16:47:17 -0600

    Finished 6.4

--- java/wiley_lance/chap6_4/P67.java
+++ java/wiley_lance/chap6_4/P67.java
@@ -1,10 +1,8 @@
-
 /**
  * Write a method that reverses the sequence of elements in an array. For example, if you call the method with the array
-
-then the array is changed to
-
-
+ * 1 4 9 1 6 9 7 4 9 11
+ * then the array is changed to
+ * 11 9 4 7 9 1 6 9 4 1
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -23,6 +21,26 @@ public class P67
         x = 0;
     }
 
+    public static void main()
+    {
+        int[] base = {1, 4, 9, 1, 6, 9, 7, 4, 9, 11};
+    
+        System.out.println("Base Length = " + base.length);
+        System.out.println("Base: 1, 4, 9, 1, 6, 9, 7, 4, 9, 11");
+        System.out.print("Output: ");
+        
+        for(int i = base.length-1; i >= 0; i--)
+        {
+            System.out.print(base[i] + ", ");
+        }
+        System.out.println();
+        System.out.println("Expected: 11, 9, 4, 7, 9, 1, 6, 9, 4, 1");
+    }
+
+    
+    
+    
+    
     /**
      * An example of a method - replace this comment with your own
      * 
