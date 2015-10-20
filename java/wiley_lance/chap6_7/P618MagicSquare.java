6b5f1711cff43dca46a4b7071d7d7af7897cd6b4
LanceEddleman <ozzland_dracor@hotmail.com>
2015-10-20 15:56:34 -0600

    Set base for 6.7
    Go back and do 6.5 and 6.6 no jumping ahead

---
+++ java/wiley_lance/chap6_7/P618MagicSquare.java
@@ -0,0 +1,43 @@
+
+/**
+ * P6.18 	
+ * Magic squares. 
+ * An  matrix that is filled with the numbers is a magic square if the sum of the elements in each row, in each column, and in the two diagonals is the same value.
+ * 16 | 3 | 2 | 13
+ * 5 | 10 | 11 | 8
+ * 9 | 6 | 7 | 12
+ * 4 | 15 | 14 | 1
+ * Write a program that reads in 16 values from the keyboard and tests whether they form a magic square when put into a  array.
+ * You need to test two features:
+ * 1. Does each of the numbers  occur in the user input?
+ * 2. When the numbers are put into a square, are the sums of the rows, columns, and diagonals equal to each other?
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class P618MagicSquare
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class P618MagicSquare
+     */
+    public P618MagicSquare()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ java/wiley_lance/chap6_7/SelfCheck35.java
@@ -0,0 +1,42 @@
+
+/**
+ * 35. 	
+Consider an 8 × 8 array for a board game:
+int[][] board = new int[8][8];
+Using two nested loops, initialize the board so that zeroes and ones alternate, as on a checkerboard:
+0 1 0 1 0 1 0 1
+1 0 1 0 1 0 1 0
+0 1 0 1 0 1 0 1
+. . .
+1 0 1 0 1 0 1 0
+Hint: Check whether i + j is even.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SelfCheck35
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SelfCheck35
+     */
+    public SelfCheck35()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
