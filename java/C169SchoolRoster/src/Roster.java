87c26928b62f748c78c99e6c1a881926058e7d97
LanceEddleman <ozzland_dracor@hotmail.com>
2015-11-04 17:33:42 -0700

    finished bad emails + tested

--- java/C169SchoolRoster/src/Roster.java
+++ java/C169SchoolRoster/src/Roster.java
@@ -13,17 +13,18 @@ public class Roster
         "2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
         "3,Jack,Napoli,The_lawyer99yahoo.com,19,85,84,87",
         "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
-        "5,Lance,Eddleman,leddlem@my.wgu.edu,45,76,85,98"};
+        "5,Lance,Eddleman,leddlem@my.wgu.edu,45,76,85,20"};
 
     static ArrayList<Student> studentArrayList = new ArrayList<Student>();
 
     public static void main(String[] args) {
-        clear();
-        studentsPrintStringArray();  // print the original string array
-        roster();                    // create new students ArrayList
-        print_all();                 // print all elements from the studentsArrayList
-        // removeStudent(2);         // remove the student ID 2
-        // print_all();              // print all elements from the studentsArrayList
+        clear();                    // clear console
+        roster();                   // create new students ArrayList
+        print_all();                // print all elements from the studentsArrayList
+        print_invalid_emails();     // prints a list of invalid emails
+        // print_average_grade();      // print all students with average grade
+        remove("3");                // remove the student with ID 3
+        remove("3");                // remove the student with ID 3
     }
 
     // Basic print for original String Array
@@ -146,30 +147,88 @@ public class Roster
         System.out.println();
     }
 
-    // 3.d Average Grades - Check with Mike to see if this is the right way to do it
-    public static void print_average_grade(int studentID) {
-        System.out.println("\033[32m average grade test print");
-        // public static void print_average_grade(String studentID) that correctly prints a student’s average grade by student ID
-        // personal touch, list studentID, first name, grades, [average] in  tabbed format, add color for average
+    // 3.d Average Grades
+    public static void print_average_grade(String studentID) {
+        // Prints a student’s average grade by student ID
+        // Personal touch, list studentID, first name, grades, [average] in  tabbed format, add color for average
+        // Add if, for the index selection, if not found message "Does not exist"
+        // Add if, less then 70 red, 70+ green
 
         System.out.println("\033[32mStudent Average Grade (Formatted)\033[0m");
-        System.out.printf("%-5s %-15s %-8s %-8s %-8s %-10s\n", "ID", "First Name", "Grade1", "Grade2", "Grade3",
-                "[Average]");
-        Student a = studentArrayList.get(studentID-1); // studentID  = ID-1 for index reference
-        System.out.printf("%-5s %-15s %4s %8s %8s %10s\n", a.getStudentID(),a.getFirstName(),a.getGrade1(),
-                a.getGrade2(),a.getGrade3(),a.getAverage());
+        System.out.printf("%-5s %-15s %-8s %-8s %-8s %-10s\n", "ID", "First Name", "Grade1", "Grade2", "Grade3", "[Average]");
 
-        // Add if, for the index selection, if it is not found or higher then size then give message out of range or
-        // does not exist
+        for (int i = 0; i < studentArrayList.size(); i++) {
+            Student a = studentArrayList.get(i);
+            if (a.getStudentID().equals(studentID)) {
+                if(a.getAverage() >= 70.0){
+                    System.out.printf("%-5s %-15s %4s %8s %8s \033[32m%10s\033[0m\n", a.getStudentID(), a.getFirstName(), a.getGrade1(),
+                            a.getGrade2(), a.getGrade3(), a.getAverage());
+                    System.out.println();
+                    return;
+                }
+                else{
+                    System.out.printf("%-5s %-15s %4s %8s %8s \033[31m%10s\033[0m\n", a.getStudentID(), a.getFirstName(), a.getGrade1(),
+                            a.getGrade2(), a.getGrade3(), a.getAverage());
+                    System.out.println();
+                    return;
+                }
+            }
+        }
+        System.out.println("Student ID: \033[31m" + studentID + "\033[0m doesn't exist");
     }
 
-    // 3.e Invalid Emails
+    // 3.e Invalid Emails  - Check with Mike to see if this is the right way to do it
     public static void print_invalid_emails()
     {
-        // Verifies student e-mail addresses, displays all invalid e-mail addresses to the user
-        // Note: valid e-mail checks: should include an at sign (“@”) and period (“.”) and doesn’t include a space.
+        // Verifies student e-mail addresses, displays all invalid e-mail addresses
+        // Note: valid e-mail checks: should include at sign (“@”) and period (“.”) - doesn’t include a space.
+        // print the ID, first name and the invalid email
 
-        // review chapter 4.7.3 for character matching to validate bad eamils
+        // search for @, if not found fail, if found, search for . if not found fail
+
+        String[] invalidEmails = new String[studentArrayList.size()];
+        String[] missingAT = new String[studentArrayList.size()];
+        String[] missingPD = new String[studentArrayList.size()];
+        int invalidCount = 0;
+
+        for (int i = 0; i < studentArrayList.size(); i++)
+        {
+            Student s = studentArrayList.get(i);
+
+            if (s.getEmailAddress().contains("@") && s.getEmailAddress().contains(".")) { }
+            else
+            {
+                invalidEmails[i] = s.getStudentID();
+                if (s.getEmailAddress().contains("@")) { }
+                else { missingAT[i] = "@"; }
+                if (s.getEmailAddress().contains(".")) { }
+                else { missingPD[i] = "."; }
+                invalidCount++;
+            }
+        }
+
+        if(invalidCount == 0)
+        { System.out.println("\033[32mAll Email Addresses appear to be valid!\033[0m"); return; }
+
+        System.out.println("\033[31mInvalid Student Email Addresses (Formatted)\033[0m");
+        System.out.printf("%-5s %-15s %-25s %-10s\n", "ID", "First Name", "Email Address", "Missing");
+
+        for (int i = 0; i < studentArrayList.size(); i++)
+        {
+            Student a = studentArrayList.get(i);
+            String missing = "";
+
+            if (missingAT[i] != null && missingPD[i] != null)
+            { missing = " @ ."; }
+            else if (missingAT[i] != null)
+            { missing = " @"; }
+            else if (missingPD[i] != null)
+            { missing = "."; }
+
+            if (a.getStudentID().equals(invalidEmails[i])) {
+                System.out.printf("%-5s %-15s %15s %5s \033[31m%-3s\033[0m\n", a.getStudentID(), a.getFirstName(), a.getEmailAddress(), "", missing);
+            }
+        }
     }
 
     // Clear console
--- java/C169SchoolRoster/src/RosterTest.java
+++ java/C169SchoolRoster/src/RosterTest.java
@@ -5,16 +5,20 @@ public class RosterTest {
 
     public static void main (String[] args) {
         Roster.roster();
-        // Roster.remove("4");
-         Roster.print_all();
-        // Roster.remove("4");
+       //  Roster.remove("2");
+       //  Roster.print_all();
+       //  Roster.remove("3");
         // Roster.print_all_tab();
-        // Roster.print_all_formatted();
-        Roster.printStudent(4);
-        Roster.print_average_grade(5);
+        Roster.print_all_formatted();
+        // Roster.printStudent(4);
+       // Roster.remove("4");
+        Roster.add("6", "Michael", "Larsen", "ml#shoot-n-Hoops#com", 40, 95, 97, 100);
+
+        Roster.print_invalid_emails();
+        // Roster.print_average_grade("5");
         // Student.color();
         // Roster.averageGrade(2);
-        // Roster.add("6", "Michael", "Larsen", "ml@shoot-n-Hoops.com", 40, 95, 97, 100);
+//         Roster.add("6", "Michael", "Larsen", "ml@shoot-n-Hoops.com", 40, 95, 97, 100);
 //        Roster.roster();
 
         // Roster.print_all();
